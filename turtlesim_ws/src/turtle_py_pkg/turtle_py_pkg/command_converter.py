230719700ba99a8b0221059fe307970e47962b76
Charlie <91991045+FujiBookPro@users.noreply.github.com>
2025-10-31 01:06:14 +0000

    Complete Activity 4

---
+++ turtlesim_ws/src/turtle_py_pkg/turtle_py_pkg/command_converter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import String
+from geometry_msgs.msg import Twist
+
+class CommandConverterNode(Node):
+    def __init__(self):
+        super().__init__("command_converter")
+        self.command_subscriber = self.create_subscription(
+            String, "/commands", self.callback_command, 10)
+        self.vel_publisher = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
+        self.get_logger().info("Command converter has been started.")
+
+    def callback_command(self, command_msg: String):
+        vel_msg = Twist()
+        if command_msg.data == "move forward":
+            vel_msg.linear.x = 1.0
+        elif command_msg.data == "move right":
+            vel_msg.linear.y = 1.0
+        else:
+            vel_msg.linear.x = 0.0
+            vel_msg.linear.y = 0.0
+
+        self.vel_publisher.publish(vel_msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CommandConverterNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ turtlesim_ws/src/turtle_py_pkg/turtle_py_pkg/move_forward.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import String
+
+class MoveForwardNode(Node):
+    def __init__(self):
+        super().__init__("move_forward_node")
+        self.command_publisher = self.create_publisher(String, "/commands", 10)
+        self.timer = self.create_timer(2.0, self.callback_command)
+        self.get_logger().info("Move forward node has been started.")
+
+    def callback_command(self):
+        msg = String()
+        msg.data = "move forward"
+        self.command_publisher.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MoveForwardNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
